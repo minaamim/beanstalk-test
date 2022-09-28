f952d406dda2d02405181f1c666913c7f0d1dfcf
minaamim <minaaa5814@gmail.com>
2022-09-28 15:39:28 +0900

    initial commit

---
+++ src/main/java/cmc/beanstalktest/BeanstalkTestApplication.java
@@ -0,0 +1,13 @@
+package cmc.beanstalktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanstalkTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanstalkTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cmc/beanstalktest/BeanstalkTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cmc.beanstalktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanstalkTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
